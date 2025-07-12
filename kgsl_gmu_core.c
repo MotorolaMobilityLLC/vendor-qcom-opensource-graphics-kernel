1ff4cc31e76e4af8f410afcb849d3af3d1f115f1
Lynus Vaz <lvaz@qti.qualcomm.com>
2025-07-11 17:35:19 -0700

    kgsl: Check for errors from mapping sg lists in the iommu
    Error returns from iommu_map_sg() were being ignored. Check for the result
    of this API correctly.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2025-47397
    
    Change-Id: I92cf4d4ce5f7a4937097b06c34406cf16ad174ef
    Signed-off-by: Lynus Vaz <lvaz@qti.qualcomm.com>
    Signed-off-by: Ashutosh Verma <ashverma@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3624101
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Sipra Patel <siprap@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- kgsl_gmu_core.c
+++ kgsl_gmu_core.c
@@ -190,7 +190,7 @@ int gmu_core_timed_poll_check(struct kgsl_device *device,
 int gmu_core_map_memdesc(struct iommu_domain *domain, struct kgsl_memdesc *memdesc,
 		u64 gmuaddr, int attrs)
 {
-	size_t mapped;
+	ssize_t mapped;
 
 	if (!memdesc->pages) {
 		mapped = kgsl_mmu_map_sg(domain, gmuaddr, memdesc->sgt->sgl,
@@ -209,7 +209,10 @@ int gmu_core_map_memdesc(struct iommu_domain *domain, struct kgsl_memdesc *memde
 		sg_free_table(&sgt);
 	}
 
-	return mapped == 0 ? -ENOMEM : 0;
+	if (!mapped)
+		mapped = -ENOMEM;
+
+	return (mapped < 0) ? mapped : 0;
 }
 
 static int gmu_core_iommu_fault_handler(struct iommu_domain *domain,
