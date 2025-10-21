d79827086e2d2be1f0a6e8c2c312e37292f0166b
Shiv Kumar <shikum@qti.qualcomm.com>
2025-10-21 18:43:50 +0530

    kgsl: gen8: Disable HLSQ stproc stc_buf_mode for gen8_9_0
    Disable HLSQ stproc stc_buf_mode for gen8_9_0.
    
    Change-Id: I9a591c5d1de8cc70838dab241e6af57ce2a17f3d
    Signed-off-by: Shiv Kumar <sh(CR)@qti.qualcomm.com>
    Reviewed-on: https://gerrit.mot.com/3663819
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Reviewed-by: Xu Zhang <zhangxu21@lenovo.com>
    Submit-Approved: Xu Zhang <zhangxu21@lenovo.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -3972,6 +3972,8 @@ static const struct gen8_nonctxt_regs gen8_9_0_nonctxt_regs[] = {
 	{ GEN8_SP_READ_SEL, 0x0001ff00, BIT(PIPE_NONE) },
 	{ GEN8_TPL1_DBG_ECO_CNTL, 0x10100000, BIT(PIPE_NONE) },
 	{ GEN8_TPL1_DBG_ECO_CNTL1, 0x04000720, BIT(PIPE_NONE) },
+	/* Disable HLSQ stproc stc_buf_mode */
+	{ GEN8_SP_HLSQ_DBG_ECO_CNTL_3, BIT(7), BIT(PIPE_NONE) },
 	{ 0 },
 };
 
